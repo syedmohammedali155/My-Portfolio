7991814aedd37dca2b84c29b8eea819d4fe0535f
Syed Muhammad Ali <syedmohammedali155@gmail.com>
2026-08-25 20:06:18 +0500

    Update vite config base to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/My-Portfolio/', // GitHub Pages white screen fix
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
